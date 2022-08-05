f0dfe4e9a7c598f1ecdd28ea54c502f9f3156a95
monji <108975217+mwnji@users.noreply.github.com>
2022-08-05 20:52:36 +0430

    Add files via upload

---
+++ report.py
@@ -0,0 +1,170 @@
+import os
+import time
+import sys
+from pyfiglet import Figlet
+import requests
+import pyfiglet
+import datetime
+w = str(datetime.datetime.now())
+green = "\033[32m"
+red = "\033[31m"
+blue = "\033[36m"
+pink = "\033[35m"
+yellow = "\033[93m"
+darkblue = "\033[34m"
+white = "\033[00m"
+# mark
+mark = '\x1b[38;5;4m' #blue
+mark1 = '\x1b[48;5;15m' # blake for back ground
+mark2 = '\x1b[0m' #end ..
+os.system("clear")
+print (f"""{mark1}{red}
+I LOVE YOU💔
+╔  {w}  ╖
+{mark2}""")
+time.sleep(0.2)
+print(f"{yellow} ")
+Sa=Figlet(font="slant")
+print(Sa.renderText("Amir"))
+print('')
+print(f"{green}")
+a = input ("installing libary ⎆ (y/n) ")
+if a == "y":
+    print()
+    os.system(" pip install colored && pip install requests &&  pip install datetime && pip install colorama && pip install pyfiglet")
+    print('')
+    print('')
+    print('')
+    print('')
+if a == "n":
+    print("\r 𝚂𝙰𝙱𝚁 𝙺𝙾𝙽  ..",end="",flush=False)
+    time.sleep(1)
+    print("\r 𝚂𝙰𝙱𝚁 𝙺𝙾𝙽  ...",end="",flush=False)
+    time.sleep(1)
+    print("\r 𝚂𝙰𝙱𝚁 𝙺𝙾𝙽  ....",end="",flush=False)
+    time.sleep(1)
+    print("\r  𝚂𝙰𝙱𝚁 𝙺𝙾𝙽  .....",end="",flush=False)
+    time.sleep(1)
+    print("\r 𝚂𝙰𝙱𝚁 𝙺𝙾𝙽        ",end="",flush=False)
+    time.sleep(0.8)
+    print("\r 𝚂𝙰𝙱𝚁 𝙺𝙾𝙽  .",end="",flush=False)
+    time.sleep(0.9)
+    print("\r 𝚂𝙰𝙱𝚁 𝙺𝙾𝙽  .. ",end="",flush=False)
+    time.sleep(0.5)
+    print("\r 𝚂𝙰𝙱𝚁 𝙺𝙾𝙽  ...",end="",flush=False)
+    time.sleep(0.7)
+    print("\r 𝚂𝙰𝙽𝚁 𝙺𝙾𝙽  ....",end="",flush=False)
+    time.sleep(1)
+    print("\r 𝚂𝙰𝙱𝚁 𝙺𝙾𝙽  .....",end="",flush=False)
+    time.sleep(0.7)
+    print("\r 𝚂𝙰𝙱𝚁 𝙺𝙾𝙽           ",end="",flush=False)
+os.system("clear")
+p = (f"""{blue}
+      ⠀⠀ ⣠⣴⣶⣶⣶⣶⣶⣤⡄⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⠉⠉⠉⠉⠉⠙⢢⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⠏⠀⠀⠀⠀ ⠀⠀⠈⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢘⡉⢻⣶nadidam⠀⢐⠀
+⠀⠀⠀⠀⠀⠀⠀⠳⡦⣸⣷⣄⡀⢀⣀⡀⠀⠀⠀⡂⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡊⣿⣿⣿⣿⣯⡩⣉⠹⢷⢦⠁⠀
+⠀⠀⠀⠀⠀⠀⠀⢰⠀⢻⣿⣿⣿⣿⣿⣿⣶⣶⡞⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⡌⠀⠀⢀⠉⠻⣿⣿⣿⣿⣿⡇⠀⠀
+⠀⣀⠠⠤⠐⠚⠱⠀⠀⠀⠈⠀⠀⠀⠉⣻⠛⠋⠀⠀⠀
+⡇⠀⠀⡊⠠⠐⠁⠀⠀⠀⠀⢰⠀⢀⠆⠿⡀⠀⠀⠀⠀
+⡗⠒⠒⠀⠀⠀⠠⠤⢤⡀⠀⢸⠀⠘⠀⠀⢌⠑⢢⠀⠀
+⡇⠠⠀⠀⠀⠀⠀⠀⠀⠀⠑⢺⠀⠐⠀⠀⠂⠀⠀⠉⠒⠢⣄
+⡇⠀⠀⠀⠀⠀⠀⠂⢰⠤⠀⠀⢦⠁⠀⠀⢂⠀⠀⠐⠄⠀⠠⠈⠉⠑⠦⡀
+⡇⠀⡀⢀⠐⡀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠐⠀⠀⠀⠀⠀⠀⠀⠁⠄⠀⠀
+""")
+for n in p:
+    sys.stdout.write(n)
+    sys.stdout.flush()
+    time.sleep(0.009)	
+time.sleep(0.5)
+# monji
+os.system("clear")
+print(" \n"*30)
+u = input(" esmet ro vared kon » ")
+os.system("clear")
+from colorama import Fore, Back, Style
+print(Fore.BLUE + '')
+
+
+k = (f"""{blue}
+
+
+⠀⠀⠀⠀⢀⣴⠟⢻⡛⢻⣦⡀
+⠀⠀⠀⠀⣸⡟⠀⠀⠀⠈⢹⡇
+⠀⠀⠀⠀⢻⣷⣶⣦⣤⣴⣾⡇
+⠀⠀⠀⠀⣸⡟⠙⠛⠛⠛⣿⡇
+⠀⠀⠀⠀⣿⡇⠀⠀⠀⢸⣿⠁
+⠀⠀⠀⠀⣿⡇⠀⠀⠀⢸⣿
+⠀⠀⠀⠀⢹⣷⠀⠀⠀⢸⣿
+⠀⠀⠀⠀⢸⡟⠀⠀⠀⢸⣿⡆
+⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⣿
+⠀⠀⠀⠀⢸⡇⠀⠀⠀⢸⡟
+⠀⠀⠀⠀⢸⡇⠀⠀⠀⢸⣷
+⠀⠀⠀⢀⣼⣇⠀⠀⠀⢸⣿⣄⣀⡀
+⠀⣤⠞⠋⠉⠁⠀⠀⠀⠀⠉⠉⠉⢻⣆
+⢸⡏⠀         ⠀⠈⠙⡇
+⠸⣇⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⣼⠃
+⠀⠙⠷⢶⣀⣀⣴⠛⠛⠷⣤⣠⡴⠞⠁
+
+""")
+for n in k:
+    sys.stdout.write(n)
+    sys.stdout.flush()
+    time.sleep(0.009)
+print()
+os.system("clear")
+print()
+time.sleep(0.5)
+b = input(f"""
+|---------------------------------|
+|      {u} ➩ `| account |         |
+|      ⎆   | exit |               |
+|      ⎆   amon id:@niroumand     |
+|---------------------------------|
+
+kalame mored nazar ro entekhab kon >> """)
+
+print(Fore.GREEN + '')
+print("\r loading server  ..",end="",flush=False)
+time.sleep(1)
+print("\r loading server  ...",end="",flush=False)
+time.sleep(1)
+print("\r loading server  ....",end="",flush=False)
+time.sleep(1)
+print("\r loading server  .....",end="",flush=False)
+time.sleep(1)
+print("\r loading server        ",end="",flush=False)
+time.sleep(0.8)
+print("\r loading server  .",end="",flush=False)
+time.sleep(0.9)
+print("\r loading server  .. ",end="",flush=False)
+time.sleep(0.5)
+print("\r loading server  ...",end="",flush=False)
+time.sleep(0.7)
+print("\r loading server  ....",end="",flush=False)
+time.sleep(1)
+print("\r loading server  .....",end="",flush=False)
+time.sleep(0.7)
+print("\r vasl shod server           ",end="",flush=False)
+print("🫂")
+if b == "account":
+    e = input("username taget vared kon >> ")
+    print ()
+    time.sleep(0.5)
+    print("darhal chek....")
+while True:
+            if b == "account":
+            	print("kolah bardari:)")
+            	print("erorr:(")
+            	print("souperbot ok")
+            	print("mostahjan")
+            	print("mostahjan")
+            	print("souperbot ok")
+            	print("kolah bardari:)")
+            	print("erorr:(")
+while True:
+            if b == "exit":
+            		print("@niroumand")
